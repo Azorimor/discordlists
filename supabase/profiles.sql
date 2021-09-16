e84fc0f1d8c1e24787a6f0ac9b1437b40955fa73
Azorimor <azorimor@gmail.com>
2021-09-16 15:23:40 +0200

    added first version for profiles table
    Signed-off-by: Azorimor <azorimor@gmail.com>

---
+++ supabase/profiles.sql
@@ -0,0 +1,33 @@
+-- Create a table for Public Profiles
+create table profiles (
+  id uuid references auth.users not null,
+  updated_at timestamp with time zone,
+  discord_username text, -- TODO maybe ref to auth.users if it is stored there?
+  discord_id text,
+  avatar_url text,
+
+  primary key (id),
+  unique(username),
+  constraint username_length check (char_length(username) >= 3)
+);
+
+alter table profiles enable row level security;
+
+create policy "Public profiles are viewable by everyone."
+  on profiles for select
+  using ( true );
+
+create policy "Users can insert their own profile."
+  on profiles for insert
+  with check ( auth.uid() = id );
+
+create policy "Users can update own profile."
+  on profiles for update
+  using ( auth.uid() = id );
+
+-- Set up Realtime!
+begin;
+  drop publication if exists supabase_realtime;
+  create publication supabase_realtime;
+commit;
+alter publication supabase_realtime add table profiles;
