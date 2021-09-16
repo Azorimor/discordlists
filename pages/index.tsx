0956b39992c666e0f324563e0f3cb3335b5adabd
Azorimor <azorimor@gmail.com>
2021-09-16 14:05:18 +0200

    fixed tailwindcss bug
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+    <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4" role="alert">
       <p className="font-bold">Be Warned</p>
       <p>Something not ideal might be happening.</p>
     </div>
