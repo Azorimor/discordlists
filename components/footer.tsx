d1543d3972a45cfa2a49b79554db377dbef0dfce
Azorimor <azorimor@gmail.com>
2021-09-20 17:01:40 +0200

    added darkmode and started design
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,10 @@
 export default function Footer() {
   return (
-    <div className="pt-12">
-      <footer id="footer" className="relative z-50 dark:bg-gray-900">
+      <footer id="footer" className="relative z-50 dark:bg-gray-800">
         <div className="py-16 flex flex-col justify-center items-center">
           <p className="focus:outline-none mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">2021
             Azorimor. All Rights Reserved.</p>
         </div>
       </footer>
-    </div>
   )
 };
