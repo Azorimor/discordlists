4ede56152fea17e26be16c8ae2dc49b218032802
Azorimor <azorimor@gmail.com>
2021-09-20 17:12:43 +0200

    changed footer
    Signed-off-by: Azorimor <azorimor@gmail.com>

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
       <footer id="footer" className="relative z-50 dark:bg-gray-800">
-        <div className="py-16 flex flex-col justify-center items-center">
-          <p className="focus:outline-none mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">2021
+        <div className="py-5 flex flex-col justify-center items-center">
+          <p className="focus:outline-none text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">2021
             Azorimor. All Rights Reserved.</p>
         </div>
       </footer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ const Home: NextPage = () => {
   return (
     <div className="flex justify-center items-center dark:bg-gray-700">
       <div className="text-center">
+        <h1 className="text-xl font-bold dark:text-white mt-5">Discord Bots and Servers</h1>
+        <p className="text-md font-light dark:text-white pb-4">Find your new favorite discord bot & server</p>
         <Searchbar />
         <p className="text-xl dark:text-white">description</p>
       </div>
