41a3b52fd87fe6fffec1c57ff7ab91e2fd20f947
Azorimor <azorimor@gmail.com>
2021-09-20 17:19:43 +0200

    added pages for bots and servers
    Signed-off-by: Azorimor <azorimor@gmail.com>

---
+++ pages/servers.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Servers: NextPage = () => {
+  return (
+    <div className="flex justify-center items-center dark:bg-gray-700">
+      <div className="text-center">
+        <h1 className="text-xl font-bold dark:text-white mt-5">Servers</h1>
+        <p className="text-md font-light dark:text-white pb-4">Find your new favorite discord bot & server</p>
+        <p className="text-xl dark:text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime ipsum vitae, ad facere inventore illo. Odio dignissimos, ipsum alias repudiandae molestiae vitae atque quis tenetur libero neque in assumenda ex eveniet consectetur quod ducimus nihil labore quidem inventore itaque reiciendis! Laudantium placeat provident suscipit numquam dignissimos consequuntur necessitatibus sequi ratione.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Servers
